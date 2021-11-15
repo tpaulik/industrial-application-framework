d65bdd3f8ad7646da76417eda1d8d2754a9c77b7
paulik <tamas.paulik@nokia.com>
2021-11-15 10:36:55 +0100

    Revert "add support of appPodFixIp and appInterfaceName pna properties"
    This reverts commit 1dfadf1d1405ea981d3f4a09b372cc5c76f4e989.

--- consul-operator/api/v1alpha1/consul_types.go
+++ consul-operator/api/v1alpha1/consul_types.go
@@ -19,12 +19,10 @@ const (
 )
 
 type PrivateNetworkAccess struct {
-	ApnUUID              string    `json:"apnUUID,omitempty"`
-	Networks             []Network `json:"networks,omitempty"`
-	CustomerNetwork      string    `json:"customerNetwork"`
-	AdditionalRoutes     []string  `json:"additionalRoutes,omitempty"`
-	NetworkInterfaceName string    `json:"networkInterfaceName,omitempty"`
-	AppPodFixIp          string    `json:"appPodFixIp,omitempty"`
+	ApnUUID          string    `json:"apnUUID,omitempty"`
+	Networks         []Network `json:"networks,omitempty"`
+	CustomerNetwork  string    `json:"customerNetwork"`
+	AdditionalRoutes []string  `json:"additionalRoutes,omitempty"`
 }
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
