bee89c964c5fd7e79e9dc67990d880fa0ea6b2a6
paulik <tamas.paulik@nokia.com>
2022-06-08 13:01:03 +0200

    add script to execute coverage measurement along with tests

---
+++ consul-operator/unittest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+COVERPKG=$(go list ./... | grep -v componenttest | tr '\n' ',' | sed 's/,//')
+go test ./componenttest -v -coverprofile=coverage_ct.out -race -covermode=atomic -json -ginkgo.v -coverpkg=$COVERPKG 2>&1 | tee test-report.json
+go test $(go list ./... | grep -v /componenttest) -v -coverprofile=coverage_ut.out -race -covermode=atomic -json -coverpkg=./pkg/...  2>&1 | tee -a test-report.json
+cat coverage_ct.out > coverage.out
+cat coverage_ut.out | grep -v "mode:" >> coverage.out
