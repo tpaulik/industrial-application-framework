2cfa95f4690bfeeaa970595657ec4104c2820eff
tpaulik <86347551+tpaulik@users.noreply.github.com>
2022-06-10 14:41:14 +0200

    Update unittest.sh
    make ut coverage optional

--- consul-operator/unittest.sh
+++ consul-operator/unittest.sh
@@ -4,4 +4,6 @@ COVERPKG=$(go list ./... | grep -v componenttest | tr '\n' ',' | sed 's/,//')
 go test ./componenttest -v -coverprofile=coverage_ct.out -race -covermode=atomic -json -ginkgo.v -coverpkg=$COVERPKG 2>&1 | tee test-report.json
 go test $(go list ./... | grep -v /componenttest) -v -coverprofile=coverage_ut.out -race -covermode=atomic -json -coverpkg=./pkg/...  2>&1 | tee -a test-report.json
 cat coverage_ct.out > coverage.out
-cat coverage_ut.out | grep -v "mode:" >> coverage.out
+if [[  $(cat coverage_ut.out | grep -v "mode:")  ]]; then
+ cat coverage_ut.out | grep -v "mode:" >> coverage.out
+fi
