9119df68289ed7873f5f2cdb56f4c2f52d5f4ce2
tpaulik <86347551+tpaulik@users.noreply.github.com>
2022-06-14 15:13:14 +0200

    Update unittest.sh
    add licence header

--- consul-operator/unittest.sh
+++ consul-operator/unittest.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright 2022 Nokia
+# Licensed under the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+
 set -eo pipefail
 COVERPKG=$(go list ./... | grep -v componenttest | tr '\n' ',' | sed 's/,//')
 go test ./componenttest -v -coverprofile=coverage_ct.out -race -covermode=atomic -json -ginkgo.v -coverpkg=$COVERPKG 2>&1 | tee test-report.json
